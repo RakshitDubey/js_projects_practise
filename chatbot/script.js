ede53083095df432a7319c474aff33f40c23f692
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2023-03-06 00:41:39 -0800

    Add files via upload

---
+++ chatbot/script.js
@@ -0,0 +1,5 @@
+const chatBody=document.querySelector('.chat-body');
+const txtinput=document.querySelector('#input');
+const send=document.querySelector('.send')
+
+send.addEventListener('click',()=>)
